b2f8fb13369a582bfe3bb502ba03a7e57bf70ea7
TracyDing7 <48873184+TracyDing7@users.noreply.github.com>
2019-06-02 22:28:23 -0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,37 @@
+// A class to represent employees in general.
+public class Employee {
+
+	private int baseHours = 40;
+	private double baseSalary = 40000.0;
+	private int baseVacationDays = 10;
+	private String baseVacationForm = "yellow";
+  
+	public int getHours() {
+		return baseHours;                // 40 hours/week
+	}
+
+	public double getSalary() {
+		return baseSalary;               // $40,000.00
+	}
+
+	public int getVacationDays() {
+		return baseVacationDays;         // 10 days
+	}
+
+	public String getVacationForm() {
+		return baseVacationForm;         // yellow
+	}	
+	
+	public final void setBaseHours(int hours) {
+		baseHours = hours;
+	}
+	public final void setBaseSalary(double salary) {
+		baseSalary = salary;
+	}
+	public final void setBaseVacationDays(int days) {
+		baseVacationDays = days;
+	}
+	public final void setBaseVacationForm(String form) {
+		baseVacationForm = form;
+	}
+}
